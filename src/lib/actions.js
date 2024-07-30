d6b57486750f487176a96d91b4b5702b29e703f7
LuchoCruz08 <lucianovcruz2004@gmail.com>
2024-07-30 17:15:53 -0300

    Troubleshooting signup

--- src/lib/actions.js
+++ src/lib/actions.js
@@ -7,8 +7,6 @@ import { createClient } from "@/utils/supabase/server";
 export async function login(formData) {
   const supabase = createClient();
 
-  // type-casting here for convenience
-  // in practice, you should validate your inputs
   const data = {
     email: formData.get("email"),
     password: formData.get("password"),
@@ -25,21 +23,22 @@ export async function login(formData) {
 }
 
 export async function signup(formData) {
-  const supabase = createClient();
+  const supabase = createClient()
 
   const data = {
-    email: formData.get("email"),
-    password: formData.get("password"),
-  };
-
-  const { error } = await supabase.auth.signUp(data);
-
-  if (error) {
-    redirect("/error");
+    email: formData.get('email'),
+    password: formData.get('password'),
   }
 
-  revalidatePath("/confirmar-correo", "layout");
-  redirect("/confirmar-correo");
+  const { error } = await supabase.auth.signUp(data)
+
+  if (error) {
+    console.error(error.message)
+    redirect('/error')
+  }
+
+  revalidatePath('/confirmar-correo', 'layout')
+  redirect('/confirmar-correo')
 }
 
 export async function signOut() {
