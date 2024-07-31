e8abbbf2ac68f412312448521a6fb647bf9d671f
LuchoCruz08 <lucianovcruz2004@gmail.com>
2024-07-31 11:50:24 -0300

    Analytics, Speed Insights

--- src/app/confirmar-correo/page.jsx
+++ src/app/confirmar-correo/page.jsx
@@ -1,6 +1,6 @@
 export default function ConfirmarCorreo() {
   return (
-    <section className="bg-gray-900 text-white">
+    <section className="bg-gray-900 text-white min-h-screen">
       <div className="mx-auto max-w-screen-xl px-4 py-32 lg:flex lg:h-screen lg:items-center">
         <div className="mx-auto max-w-3xl text-center">
           <p className="mx-auto mt-4 max-w-xl sm:text-xl/relaxed">
--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,7 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -11,7 +13,11 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" suppressHydrationWarning>
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        {children}
+        <Analytics/>
+        <SpeedInsights/>
+        </body>
     </html>
   );
 }
